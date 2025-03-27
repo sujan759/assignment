10f94180978d64fc7464c61f5f5523403bb3134b
Sujan <sujansahu655@gmail.com>
2025-03-27 23:03:20 +0530

    level1

---
+++ assignment2 level1/fun.dart
@@ -0,0 +1,13 @@
+import 'dart:io';
+
+
+import 'dart:io';
+
+//  input function
+
+String strinValue()=>stdin.readLineSync()!;
+num numValue()=>num.parse(strinValue());
+int intValue()=>int.parse(strinValue());
+double doubleValue()=>double.parse(strinValue());
+
+
--- assignment2 level1/math_multipleOperation.dart
+++ assignment2 level1/math_multipleOperation.dart
@@ -1,2 +1,27 @@
 import 'dart:math';import 'dart:io';
 
+import 'fun.dart';
+void main(){
+  const double rate=5.6;
+  double principle=10000;
+  int time=2;
+  double simpleIntrest=(principle*rate*time)/100;
+  print("Simple itrest calculate: $simpleIntrest");
+
+  print("Enter a number find out squre :");
+  int num=int.parse(stdin.readLineSync()!);
+  print("Squre a number : ${num*num}");
+
+  print("Enter Two num :");
+  int num1=intValue();
+  int num2=intValue();
+  print("Qutient: ${num1~/num2}, Remender :${num1%num2}");
+
+///Sweaping Two num
+  print("Enter befor swpp :num1=$num1 ,num2=$num2");
+  int temp=num1;
+  num1=num2;
+  num2=temp;
+  print("After swap num :  num1=$num1, num2=$num2");
+
+}
