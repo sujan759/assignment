116b25dc471fdb77845bf29bbebe26a01b66ebec
Sujan <sujansahu655@gmail.com>
2025-03-29 09:15:24 +0530

    data structure

---
+++ assignment2 level1/datastructure.dart
@@ -0,0 +1,9 @@
+import 'dart:io';
+
+void main(){
+  List<int> num=[12,4,1,2,1,2,2,4];
+  print('Rsult are in store num : $num');
+
+  Map<String,String> phonPric={'poco':'12000', 'relme':'14025'};
+  print("phone price store: $phonPric");
+}
