b983e1e3e4aa0e3869f37b6970545534baaaeed0
Sujan <sujansahu655@gmail.com>
2025-03-29 09:15:56 +0530

    math manipulation

--- assignment2 level1/math_multipleOperation.dart
+++ assignment2 level1/math_multipleOperation.dart
@@ -2,7 +2,7 @@ import 'dart:math';import 'dart:io';
 
 import 'fun.dart';
 void main(){
-  const double rate=5.6;
+/*  const double rate=5.6;
   double principle=10000;
   int time=2;
   double simpleIntrest=(principle*rate*time)/100;
@@ -23,5 +23,72 @@ void main(){
   num1=num2;
   num2=temp;
   print("After swap num :  num1=$num1, num2=$num2");
+
+  ///bil split calculation
+  print("Enter total  bill amount:");
+  int bill=intValue();
+  print("Enter total pepole:");
+  int people=intValue();
+  print("Each persion payes: ${bill/people}");
+
+  ///time calculation
+  print("Enter distance in travel:");
+  double distance=doubleValue();
+  print("Enter speed in km/hr:");
+  double speed=doubleValue();
+  print("Time calculate cover the distance : ${distance/speed}");
+
+  ///cheking the number even and odd number
+  print("Enter a number : ");
+  int check=intValue();
+  print("Check even or odd in the number :");
+  print(check%2==0? "Even":"Odd");
+
+  ///sum off natural number
+
+///mutiplecation table
+
+  print("Enter a number to generate multiplication table:");
+  int tableNum=intValue();
+
+  for(int i=1; i<10;i++){
+    print("Table $tableNum*$i= ${tableNum*i}");
+  }
+
+  ///power calculation
+  ///
+  print('Enter base and exponent');
+  int base=intValue();
+  int exponent=intValue();
+  double result=pow(base, exponent).toDouble(); //calculate power convert result
+  print("$base^ $exponent=$result");
+
+  */
+  /// simple calculater using switch case
+  ///
+  print("Entger two number:");
+  int num1=intValue();
+  int num2=intValue();
+  print("Enter an operation:( '+', '*','-','/', '%')  ");
+  String op=strinValue();
+  switch (op){
+    case '+':
+      print("Result :${num1+num2}");
+      break;
+
+    case '-':
+      print("Result :${num1-num2}");
+      break;
+    case '*':
+    print("Result :${num1*num2}");
+    break;
+    case '/':
+    print("Result :${num1/num2}");
+    break;
+    case '%':
+    print("Result :${num1%num2}");
+    break;
+  }
+
 
 }
